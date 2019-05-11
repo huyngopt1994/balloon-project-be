4c3ffce00a71e15dc466eaca5f1e2e382e09db42
vuhuy@inspectorio.com <huyngopt1994@gmail.com>
2019-05-11 19:19:36 +0700

    Init models and progresql connection

--- ballon/web_source/models.py
+++ ballon/web_source/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
+
 # Create your models here.
+class Users(models.Model):
+    name = models.CharField(max_length=255)
+    telephone = models.IntegerField()
+    tax_number = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Products(models.Model):
+    price = models.IntegerField()
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField()
+    image = models.ImageField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Discounts(models.Model):
+    percentage = models.IntegerField(unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Transactions(models.Model):
+    total = models.IntegerField()
+    type = models.CharField(max_length=255)
+    transport_fee = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(Users, on_delete=models.CASCADE)
+    product = models.ForeignKey(Products, on_delete=models.CASCADE)
+    discount = models.ForeignKey(Discounts, on_delete=models.CASCADE)
